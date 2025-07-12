631aa67e6f03c01cfc8fc0e1a8d024765aba6506
a-mariam <mariiam22222@gmail.com>
2025-07-12 13:42:35 +0100

    worked on login ui

--- src/features/auth/login.tsx
+++ src/features/auth/login.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import Image, {StaticImageData} from 'next/image';
-import {Ave} from "@/app/fonts";
+import Image from 'next/image';
+import {AvenirNext600,AvenirNext700} from "@/app/fonts";
+import AuthInputField from "@/components/Input";
+import {Button} from "@/components/ui/button";
+import {useRouter} from "next/navigation";
 
 const Login = () => {
-    return (
-        <div className={` w-full h-full bg-blue-600 grid grid-cols-2`}>
-            {/*bg-[#fefefe]*/}
-            <div className={` w-full pt-25 pl-6 h-full bg-[#fefefe] `}>
 
-                <div className={`w-[80%]  mr-auto ml-auto mb-30`}>
+    const router = useRouter();
+    const handleClick = () => {
+        router.push("/users");
+    }
+    return (
+        <div className={` w-screen h-screen  grid md:grid-cols-2`}>
+            {/*bg-[#fefefe]*/}
+            <div className={` w-full hidden md:grid lg:grid md:pl-6 h-full  bg-[#fdfdfd] `}>
+
+                <div className={`w-[80%]  mt-26 mr-auto ml-auto `}>
                     <Image
                         src={`/Group.svg`}
                         width={180}
@@ -17,7 +25,7 @@ const Login = () => {
                         alt={`Logo`}
                     />
                 </div>
-               <div className={` w-[85%] mr-auto ml-auto `}>
+               <div className={` w-[85%] mr-auto mb-6 ml-auto `}>
                    <Image
                        src={`/pablo-sign-in 1.svg`}
                        alt="circle"
@@ -30,11 +38,24 @@ const Login = () => {
                    />
                </div>
             </div>
-            <div className={` w-full h-full pl-10 bg-[#ffffff] grid pt-40 `}>
-                <div className={`h-fit ${Ave.className}  `}>
-                    <p className={`text-[#213F7D] text-[40px]`}>Welcome!</p>
+            <div className={` w-[100%] h-full bg-[#ffffff] grid  md:pl-28 px-6   `}>
+                <div className={`w-[100%] md:place-self-center pt-10 h-fit grid gap-14   `}>
+                    <div className={` w-fit h-fit  `}>
+                        <p id={'welcomeText'} data-testid={'welcomeText'} className={`${AvenirNext700.className} mb-2 leading-tight text-[#213F7D] text-[40px]`}>Welcome!</p>
+                        <p className={`text-[16px]  mr-auto ml-auto text-[#545F7D] `}>Enter details to login.</p>
+                    </div>
+                    <form
+                        id={'loginForm'}
+                        data-testid={'loginForm'}
+                        className={`w-full h-fit  grid gap-5 `}>
+                        <div className={` h-fit grid gap-5`}>
+                            <AuthInputField height={'h-[3rem]'} width={'w-[80%] '} label={'Email'} id={'userEmailInput'}/>
+                            <AuthInputField height={'h-[3rem]'} endAdornment={''} width={'w-[80%] '} label={'Password'} id={'userPasswordInput'}/>
+                        </div>
+                        <p className={`text-[#39CDCC] ${AvenirNext600.className} text-[12px] mt-2 tracking-wide mb-2 `}>FORGOT PASSWORD?</p>
+                        <Button onClick={handleClick} className={`bg-[#39CDCC] hover:bg-[#39CDCC] w-[80%] h-fit py-3  text-white`}>LOG IN</Button>
+                    </form>
                 </div>
-
             </div>
         </div>
     );
