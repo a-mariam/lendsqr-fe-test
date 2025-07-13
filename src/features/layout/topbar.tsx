8b7d186faa4a0f2b9b68a1a3901fd294eb24c6a3
a-mariam <mariiam22222@gmail.com>
2025-07-13 13:44:56 +0100

    done working on search ui

--- src/features/layout/topbar.tsx
+++ src/features/layout/topbar.tsx
@@ -7,29 +7,30 @@ import {MagnifyingGlassIcon} from "@radix-ui/react-icons"
 const Topbar = () => {
     return (
         <div className={` w-screen bg-white sticky grid grid-cols-2 top-0 h-[13vh] shadow-md `}>
-            <div className={`w-[50vw] bg-pink-400 pl-10 h-fit self-center flex justify-between `}>
+            <div className={`w-[50vw]  pl-6 h-fit self-center flex justify-between `}>
                 <div className={`  `}>
                     <Image
                         src={`/Group.svg`}
-                        width={210}
+                        width={100}
                         height={160}
                         loading="lazy"
                         alt={`Logo`}
-                        className={`w-full h-full`}
+                        className={`w-[90%] h-full`}
                     />
                 </div>
 
                 <div id={`input-container-`}
-                     className={`flex items-center w-[50%] bg-white h-fit py-4 gap-2 rounded-sm border-2 border-[#545F7D26]  `}>
+                     // dir="ltr"
+                     className={`flex items-center w-[58%]  h-  gap-0 rounded-sm border-1 border-[#545F7D26]  `}>
                     <Input
                         id={'inputField'}
                         placeholder={'Search for anything'}
                         // type={isPasswordVisible ? 'text' : type}
-                        className={`${inter.className} focus-visible:ring-0  r h-full w-full border-0 text-[#cdd1da] text-[12px] bg-white placeholder:text-[14px] placeholder:text-[#cdd1da] data-[placeholder]:bg-[#cdd1da] data-[placeholder]:text-[8px] focus:outline-none`}
+                        className={`${inter.className} focus-visible:ring-0 bg-white  h- w-full border-0 text-[#cdd1da] text-[12px]  placeholder:text-[14px] placeholder:text-[#cdd1da] `}
 
                     />
-                    <div className={` bg-[]`}>
-                        <MagnifyingGlassIcon/>
+                    <div className={` bg-[#39CDCC] h-[100%] rounded-r-sm grid w-fit px-5 `}>
+                        <MagnifyingGlassIcon className={`h-4 self-center w-4`} color={'white'}/>
                     </div>
                 </div>
 
