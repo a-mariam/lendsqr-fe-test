49cfdc2f86279aedccb00662c381756703bb9249
a-mariam <mariiam22222@gmail.com>
2025-07-13 11:49:30 +0100

    installed react icon and radix icon

--- src/features/layout/topbar.tsx
+++ src/features/layout/topbar.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Image from "next/image";
 import {Input} from "@/components/ui/input";
 import {inter} from "@/app/fonts";
+import {MagnifyingGlassIcon} from "@radix-ui/react-icons"
 
 const Topbar = () => {
     return (
@@ -19,22 +20,23 @@ const Topbar = () => {
                 </div>
 
                 <div id={`input-container-`}
-                     className={`flex items-center w-[50%] h-fit py-4 gap-2 rounded-sm border-2 border-[#545F7D26]  `}>
+                     className={`flex items-center w-[50%] bg-white h-fit py-4 gap-2 rounded-sm border-2 border-[#545F7D26]  `}>
                     <Input
                         id={'inputField'}
                         placeholder={'Search for anything'}
                         // type={isPasswordVisible ? 'text' : type}
-                        className={`${inter.className} focus-visible:ring-0  h-full w-full border-0 text-[#cdd1da] text-[12px] bg-white placeholder:text-[14px] placeholder:text-[#cdd1da] data-[placeholder]:bg-[#cdd1da] data-[placeholder]:text-[8px] focus:outline-none`}
+                        className={`${inter.className} focus-visible:ring-0  r h-full w-full border-0 text-[#cdd1da] text-[12px] bg-white placeholder:text-[14px] placeholder:text-[#cdd1da] data-[placeholder]:bg-[#cdd1da] data-[placeholder]:text-[8px] focus:outline-none`}
 
                     />
                     <div className={` bg-[]`}>
-
+                        <MagnifyingGlassIcon/>
                     </div>
                 </div>
 
             </div>
         </div>
     );
+
 };
 
 export default Topbar;
