8df1826c847bd8cf775842140446f4101db4f353
a-mariam <mariiam22222@gmail.com>
2025-07-21 09:12:37 +0100

    added persistor gate to provider

--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -1,13 +1,16 @@
 'use client';
 
 import React from 'react'
-import { store } from '@/redux/store';
+import {persistor, store} from '@/redux/store';
 import { Provider } from 'react-redux';
+import {PersistGate} from "redux-persist/integration/react";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
     return (
-            <Provider store={store}>
-                {children}
+            <Provider store={store} >
+                <PersistGate loading={null} persistor={persistor}>
+                   {children}
+                </PersistGate>
             </Provider>
     )
 }
